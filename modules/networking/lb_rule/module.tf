9074758c1cb1fcfa2a28b63d8f663e224515ad1d
Neil Fawcett <neil.fawcett@flex.com>
2022-10-21 12:31:43 -0700

    change values of backend_address_pool_ids and probe_id in modules/networking/lb_rule/module.tf

--- modules/networking/lb_rule/module.tf
+++ modules/networking/lb_rule/module.tf
@@ -17,11 +17,13 @@ resource "azurerm_lb_rule" "lb" {
   protocol                       = var.settings.protocol
   frontend_port                  = var.settings.frontend_port
   backend_port                   = var.settings.backend_port
-  backend_address_pool_ids       = try(var.settings.backend_address_pool_ids, null)
-  probe_id                       = try(var.settings.probe_id, null)
-  enable_floating_ip             = try(var.settings.enable_floating_ip, null)
-  idle_timeout_in_minutes        = try(var.settings.idle_timeout_in_minutes, null)
-  load_distribution              = try(var.settings.load_distribution, null)
-  disable_outbound_snat          = try(var.settings.disable_outbound_snat, null)
-  enable_tcp_reset               = try(var.settings.enable_tcp_reset, null)
+  #backend_address_pool_ids       = try(var.settings.backend_address_pool_ids, null)
+  #probe_id                       = try(var.settings.probe_id, null)
+  backend_address_pool_ids = [can(var.settings.backend_address_pool.id) ? var.settings.backend_address_pool.id : var.remote_objects.lb[try(var.settings.loadbalancer.lz_key, var.client_config.landingzone_key)][var.settings.loadbalancer.key].id]
+  probe_id                 = can(var.settings.probe.id) ? var.settings.probe.id : var.remote_objects.lb[try(var.settings.loadbalancer.lz_key, var.client_config.landingzone_key)][var.settings.loadbalancer.key].id
+  enable_floating_ip       = try(var.settings.enable_floating_ip, null)
+  idle_timeout_in_minutes  = try(var.settings.idle_timeout_in_minutes, null)
+  load_distribution        = try(var.settings.load_distribution, null)
+  disable_outbound_snat    = try(var.settings.disable_outbound_snat, null)
+  enable_tcp_reset         = try(var.settings.enable_tcp_reset, null)
 }
