888a47f33b0ca896f2d86c7229c46c388b616bda
Neil Fawcett <neil.fawcett@flex.com>
2022-10-23 08:47:56 -0700

    corrected lb_rule module values for backend_address_pool_ids and probe_id

--- modules/networking/lb_rule/module.tf
+++ modules/networking/lb_rule/module.tf
@@ -17,8 +17,8 @@ resource "azurerm_lb_rule" "lb" {
   protocol                       = var.settings.protocol
   frontend_port                  = var.settings.frontend_port
   backend_port                   = var.settings.backend_port
-  backend_address_pool_ids       = [can(var.settings.backend_address_pool.id) ? var.settings.backend_address_pool.id : var.remote_objects.lb[try(var.settings.loadbalancer.lz_key, var.client_config.landingzone_key)][var.settings.loadbalancer.key].id]
-  probe_id                       = can(var.settings.probe.id) ? var.settings.probe.id : var.remote_objects.lb[try(var.settings.loadbalancer.lz_key, var.client_config.landingzone_key)][var.settings.loadbalancer.key].id
+  backend_address_pool_ids       = [can(var.settings.backend_address_pool.id) ? var.settings.backend_address_pool.id : var.remote_objects.backend_address_pool[try(var.settings.backend_address_pool.lz_key, var.client_config.landingzone_key)][var.settings.backend_address_pool.key].id]
+  probe_id                       = can(var.settings.lb_probe.id) ? var.settings.lb_probe.id : var.remote_objects.lb_probe[try(var.settings.lb_probe.lz_key, var.client_config.landingzone_key)][var.settings.lb_probe.key].id
   enable_floating_ip             = try(var.settings.enable_floating_ip, null)
   idle_timeout_in_minutes        = try(var.settings.idle_timeout_in_minutes, null)
   load_distribution              = try(var.settings.load_distribution, null)
